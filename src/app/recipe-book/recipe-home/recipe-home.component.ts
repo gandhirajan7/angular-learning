61b38eb498c282b6c121df7d35bdfff3b9d877f1
gandhi <gandhirajan1997@gmail.com>
2024-01-29 18:59:41 -0400

    modified recipe list, recipe detail

--- src/app/recipe-book/recipe-home/recipe-home.component.ts
+++ src/app/recipe-book/recipe-home/recipe-home.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-home',
   templateUrl: './recipe-home.component.html',
-  styleUrls: ['./recipe-home.component.css']
+  styleUrls: ['./recipe-home.component.css'],
 })
 export class RecipeHomeComponent {
+  constructor() {}
 
+  ngOnInit() {}
 }
--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
+``;
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[];
+  constructor() {
+    this.recipes = [
+      new Recipe(
+        'Briyani',
+        'Thalapakkati Mutton Briayni',
+        'https://www.spiceindiaonline.com/wp-content/uploads/2017/09/Thalappakatti-Mutton-Biriyani-3.jpg'
+      ),
+      new Recipe(
+        'Dosai',
+        'Spicy tamil masala dosai',
+        'https://www.vegrecipesofindia.com/wp-content/uploads/2021/06/masala-dosa-recipe-1-1024x1536.jpg'
+      ),
+      new Recipe(
+        'Thayir Sadham',
+        'South Indian Curd Rice',
+        'https://www.vegrecipesofindia.com/wp-content/uploads/2016/07/curd-rice-5.jpg'
+      ),
+      new Recipe(
+        'Dosai',
+        'Spicy tamil masala dosai',
+        'https://www.vegrecipesofindia.com/wp-content/uploads/2021/06/masala-dosa-recipe-1-1024x1536.jpg'
+      ),
+      new Recipe(
+        'Briyani',
+        'Thalapakkati Mutton Briayni',
+        'https://www.spiceindiaonline.com/wp-content/uploads/2017/09/Thalappakatti-Mutton-Biriyani-3.jpg'
+      ),
+    ];
+  }
 }
